3a833b7a3eb083ebf39d5e937b908aa00fb9c590
Agustina Armentano <armentanoagustina8@gmail.com>
2023-03-14 10:58:34 -0300

    agrego un client de sw

---
+++ plugins/sw.client.js
@@ -0,0 +1,17 @@
+
+export default (context) => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      for (const worker of registrations) {
+        console.log('Service worker desde el client:', worker)
+      }
+    });
+    navigator.serviceWorker.addEventListener('message', event => {
+      console.log('Mensaje recibido desde el Service Worker:', event.data);
+      context.store.commit('SET_SYNC', true);
+      setTimeout(() => {
+        context.store.commit('SET_SYNC', false);
+      }, "5000");
+    });
+  }
+}
